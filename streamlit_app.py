177cc4ef08a62e34d0e2d2a61219dc47c6c8f62c
rileycochrell <rileycochrell@gmail.com>
2025-10-29 20:39:19 -0600

    Implement EJI dataset selection for counties
    Added functionality to select and display Environmental Justice Index data for New Mexico counties.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,45 @@
 import streamlit as st
 
 st.title("Environmental Justice Index Visualization (NM)")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-options = ["Option A", "Option B", "Option C"]
-selected_option = st.selectbox("Choose an option:", options)
-st.write(f"You selected: {selected_option}")
-if option == "Option A":
-    st.write("You selected Option A. Here is some content specific to Option A.")
-    # You can add more Streamlit widgets here, like st.text_input, st.button, etc.
-    st.image("https://via.placeholder.com/150", caption="Image for Option A")
-elif option == "Option B":
-    st.write("You selected Option B. This content is for Option B.")
-    st.dataframe({"Column 1": [1, 2], "Column 2": ["X", "Y"]})
-else:
-    st.write("You selected Option C. Another set of content appears here.")
-    st.code("print('Hello from Option C!')", language="python")
+st.write("The EJI is a value representing a percentile ranking relative to communities nationwide.")
+st.write("The value shows the percentage of the nation which this")
+# perameter1 is to view the eji of that, so EJI of County or EJI of New Mexico
+counties = [
+"Bernalillo County", "Chaves County", "Cibola County", "Curry County",
+"Doña Ana County", "Eddy County", "Grant County", "Lincoln County",
+"Luna County", "McKinley County", "Mora County", "Otero County",
+"Rio Arriba County", "Sandoval County", "San Juan County", "San Miguel County",
+"Santa Fe County", "Sierra County", "Taos County", "Valencia County",
+"Catron County", "Hidalgo County", "Los Alamos County", "Colfax County",
+"Lea County", "Socorro County", "Torrance County", "Guadalupe County",
+"Harding County", "Roosevelt County", "De Baca County", "Quay County",
+"Union County"
+]
+states = [
+"Alabama",    "Arizona",    "Alaska",    "Arkansas",
+"California",    "Colorado",    "Florida",    "Connecticut",
+"Delaware",    "Texas",    "Washington",    "West Virginia",
+"Wisconsin",    "District of Columbia",    "Georgia",    "Virginia",
+"Illinois",    "Hawaii",    "Indiana",    "Idaho",
+"Kentucky",    "Iowa",    "Maine",    "Maryland",
+"Wyoming",    "American Samoa",    "Guam",    "Commonwealth of the Northern Mariana Islands",
+"Kansas",    "Puerto Rico",    "Louisiana",    "Massachusetts",
+"Michigan",    "Minnesota",    "Mississippi",    "Missouri",
+"Nevada",    "New Hampshire",    "Montana",    "Nebraska",
+"New Jersey",    "New Mexico",    "New York",    "North Carolina",
+"Ohio",    "Pennsylvania",    "North Dakota",    "Oklahoma",
+"Oregon",    "Rhode Island",    "South Carolina",    "Tennessee",
+"South Dakota",    "Utah",    "Vermont",    "United States Virgin Islands"
+]
+
+perameter1 = ["County", "New Mexico"]
+selected_perameter1 = st.selectbox("View EJI Dataset of:", perameter1)
+st.write(f"You selected {selected_perameter1} vs United States")
+if selected_perameter1 == "County":
+	selected_County1 = st.selectbox("Select a New Mexico County:", counties)
+
+
+	
+	
+else selected_parameter1 == "New Mexico":
+	
